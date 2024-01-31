5ab1850456c76700fac1431adb39fae5a513b825
kgorna <karolinagorna@hotmail.fr>
2024-01-31 11:56:45 +0100

    Remove target directory from Git tracking

--- target/debug/build/z3-sys-76687c96667b45b0/build_script_build-76687c96667b45b0.d
+++
@@ -1,5 +0,0 @@
-/home/kgorna/Documents/test/z3-tester/target/debug/build/z3-sys-76687c96667b45b0/build_script_build-76687c96667b45b0: /home/kgorna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/z3-sys-0.6.3/build.rs
-
-/home/kgorna/Documents/test/z3-tester/target/debug/build/z3-sys-76687c96667b45b0/build_script_build-76687c96667b45b0.d: /home/kgorna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/z3-sys-0.6.3/build.rs
-
-/home/kgorna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/z3-sys-0.6.3/build.rs:
--- target/debug/z3-tester.d
+++
@@ -1,1 +0,0 @@
-/home/kgorna/Documents/test/z3-tester/target/debug/z3-tester: /home/kgorna/Documents/test/z3-tester/src/main.rs
